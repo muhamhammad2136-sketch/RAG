aa2a49b489ddba451d788a4ad959070e6abab100
Muhammad-HammadUddin <hammaduddin083@gmail.com>
2026-07-24 16:42:55 +0500

    commited

--- config/llm.js
+++ config/llm.js
@@ -4,25 +4,11 @@ import { ChatGroq } from "@langchain/groq";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 
-// export const llm = new ChatGroq({
-//   apiKey: env.GROQ_API_KEY,
-//   model: "llama-3.3-70b-versatile",
-//   temperature: 0.2,
-//   maxRetries: 2,
-
-// });
-// export const llm = new ch({
-//   apiKey: env.GROQ_API_KEY,
-//   model: "llama-3.3-70b-versatile",
-//   temperature: 0.2,
-//   maxRetries: 2,
-
-
-// });
-export const llm = new ChatGoogleGenerativeAI({
-  apiKey: env.GOOGLE_API_KEY,
-  model: "gemini-2.5-flash",
-  temperature: 0.1,
-  maxOutputTokens: 150, // ya 100
+export const llm = new ChatGroq({
+  apiKey: env.GROQ_API_KEY,
+  model: "llama-3.3-70b-versatile",
+  temperature: 0.2,
   maxRetries: 2,
+  maxOutputTokens: 150
+
 });
